ebcbc315d7a99911a0727f33e871fbd591c33b46
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:57:47 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace App15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double N, M, A, B, X;
+            string inputData = Console.ReadLine(); // citim primul număr
+            N = int.Parse(inputData);
+            inputData = Console.ReadLine(); // citim al doilea număr
+            M = int.Parse(inputData);
+            inputData = Console.ReadLine(); // citim al treilea număr
+            A = int.Parse(inputData);
+            inputData = Console.ReadLine(); // citim al patrulea număr
+            B = int.Parse(inputData);
+
+
+            X = Math.Ceiling(((N * M) + (15 * (N * M))/100) / (A * B));
+
+            Console.WriteLine(X);
+        }
+    }
+}
